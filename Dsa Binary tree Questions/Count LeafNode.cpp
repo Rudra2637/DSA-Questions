a7ccd3d70f834658920ead592b21ac8c2515b77d
Rudra Pratap Singh <145354880+Rudra2637@users.noreply.github.com>
2025-02-11 19:24:02 +0000

    Updated

---
+++ Dsa Binary tree Questions/Count LeafNode.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void inOrder(BinaryTreeNode<int> *root,int &cnt){
+    if(root == NULL)return ;
+
+    inOrder(root->left,cnt);
+    if(root->left == NULL && root->right == NULL)cnt++;
+    inOrder(root->right,cnt);
+
+}
+int noOfLeafNodes(BinaryTreeNode<int> *root){
+    // Write your code here.
+    int cnt = 0;
+    inOrder(root,cnt);
+    return cnt;
+}
