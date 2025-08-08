7b3fe3c7fac435db63eda38d0bb8d6cc5db2eb11
Rudra Pratap Singh <145354880+Rudra2637@users.noreply.github.com>
2025-08-08 22:03:23 +0530

    K-th Largest Sum Subarray.cpp

---
+++ Heap DSA problem/K-th Largest Sum Subarray.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int getKthLargest(vector<int> &arr, int k)
+{
+	//	Write your code here.
+	priority_queue<int, vector<int>, greater<int>> min_heap;
+
+	for(int i=0;i<arr.size();i++){
+		int sum = 0;
+		for(int j=i;j<arr.size();j++){
+			sum += arr[j];
+			// min_heap.push(sum);
+			if(min_heap.size() < k){
+				min_heap.push(sum);
+			}
+			else {
+				if(min_heap.top() < sum){
+					min_heap.pop();
+					min_heap.push(sum);
+				}
+			}
+		}
+		
+	}
+	// while(min_heap.size() != k)min_heap.pop();
+	
+
+	return min_heap.top();
+}
