07d42338157217d5af92eb0dbbd63b9b68676bbd
Rudra2637 <singhrudra2637@gmail.com>
2025-08-11 23:13:56 +0530

    Added Heap

---
+++ Heap DSA problem/215. Kth Largest Element in an Array.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        // priority_queue<int>pq;
+        // for(int i=0;i<nums.size();i++){
+        //     pq.push(nums[i]);
+        // }
+
+        // while(k > 1){
+        //     k--;
+        //     pq.pop();
+        // }
+        priority_queue<int,vector<int>,greater<int>>pq;
+        for(int i=0;i<nums.size();i++) pq.push(nums[i]);
+        while(pq.size() != k)pq.pop();
+        return pq.top();
+    }
+};
