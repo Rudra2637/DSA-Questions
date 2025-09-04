0e1f202e14fe3a90f5129acfa67f621fa01c295f
Rudra2637 <singhrudra2637@gmail.com>
2025-09-04 22:47:42 +0530

    Added Potd

---
+++ RandomDump/3516. Find Closest Person.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int findClosest(int x, int y, int z) {
+        return abs(z-x) == abs(z-y) ? 0 : abs(z-x) > abs(z-y) ? 2:1; 
+    }
+};
