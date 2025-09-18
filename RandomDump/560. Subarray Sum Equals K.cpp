d067ac2cb04c6b7085fd302ea534b918ed23624e
Rudra2637 <singhrudra2637@gmail.com>
2025-09-18 23:28:49 +0530

    Added

---
+++ RandomDump/560. Subarray Sum Equals K.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int sum = 0;
+        int cnt = 0;
+        
+        unordered_map<int,int>m;
+        m[sum]++;
+        for(int i = 0;i<nums.size();i++){
+            sum += nums[i] ;
+            if(m.find(sum - k) != m.end()){
+                cnt += m[sum-k];
+            } 
+            m[sum]++;
+            
+        }
+        return cnt;
+    }
+};
+//1 3 6
