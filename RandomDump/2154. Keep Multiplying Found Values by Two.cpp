012a210fb0026c38edb11839af2b3dfc65288513
Rudra2637 <singhrudra2637@gmail.com>
2025-11-19 10:26:21 +0530

    Added Potd

---
+++ RandomDump/2154. Keep Multiplying Found Values by Two.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int findFinalValue(vector<int>& nums, int original) {
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size();i++){
+            if(original == nums[i])original *= 2;
+        }
+        return original;
+    }
+};
